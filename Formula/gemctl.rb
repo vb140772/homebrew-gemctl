2fa84aa3ab74df05be102454bc3deb545a805f71
Valery Bezrukov <valery.bezrukov@gmail.com>
2025-10-26 18:02:56 -0500

    Update gemctl formula SHA256 checksum for version 1.2.0

--- Formula/gemctl.rb
+++ Formula/gemctl.rb
@@ -2,7 +2,7 @@ class Gemctl < Formula
   desc "CLI tool for Gemini Enterprise management"
   homepage "https://github.com/vb140772/gemctl-go"
   url "https://github.com/vb140772/gemctl-go/releases/download/v1.2.0/gemctl_1.2.0_darwin_arm64.tar.gz" # or .tar.gz
-  sha256 "ed8b45a6d9dfd8468c287a809b6ff8a3dc1d0a99befd45f04beab0dde19bb677"
+  sha256 "df3b8e1fae6ca2863e0246e97ff0cbc8aeccb99fcafc260d03d9538e40f1216f"
   version "1.2.0"
   license "MIT"
 
