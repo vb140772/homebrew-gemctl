07b8d9a98a959d8675379fd98d63c8d31cfcf414
Valery Bezrukov <valery.bezrukov@gmail.com>
2025-10-26 17:53:59 -0500

    Update gemctl formula version to 1.2.0

--- Formula/gemctl.rb
+++ Formula/gemctl.rb
@@ -3,7 +3,7 @@ class Gemctl < Formula
   homepage "https://github.com/vb140772/gemctl-go"
   url "https://github.com/vb140772/gemctl-go/releases/download/v1.2.0/gemctl_1.2.0_darwin_arm64.tar.gz" # or .tar.gz
   sha256 "ed8b45a6d9dfd8468c287a809b6ff8a3dc1d0a99befd45f04beab0dde19bb677"
-  version "0.1.0"
+  version "1.2.0"
   license "MIT"
 
   def install
